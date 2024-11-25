50e25a914a3423f0d8ff61f4b84e0af6bd41c320
Shbc314159 <shbc99@gmail.com>
2024-11-25 14:47:04 +0000

    fixed

---
+++ viewport.js
@@ -0,0 +1,11 @@
+function setViewportUnits() {
+    let vh = window.innerHeight * 0.01;
+    let vw = window.innerWidth * 0.01;
+    document.documentElement.style.setProperty('--vh', `${vh}px`);
+    document.documentElement.style.setProperty('--vw', `${vw}px`);
+  }
+  
+  setViewportUnits();
+  window.addEventListener('resize', setViewportUnits);
+  
+  
