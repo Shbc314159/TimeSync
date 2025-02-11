76e5deba3787d3e4002ec3aacd5cd036f94f7828
Shbc314159 <shbc99@gmail.com>
2025-02-11 09:50:13 +0000

    react

--- display_calendar.js
+++ display_calendar.js
@@ -1,174 +1,92 @@
-var root = null;
+import { createRoot } from 'react-dom/client';
 
-function Title(_ref) {
-    var text = _ref.text;
+let root = null;
 
-    return React.createElement(
-        "h1",
-        { id: "title" },
-        text
-    );
+function Title({ text }) {
+    return <h1 id="title">{text}</h1>;
 }
 
-function BoxNumber(_ref2) {
-    var firstDay = _ref2.firstDay,
-        boxnum = _ref2.boxnum,
-        numDays = _ref2.numDays;
-
+function BoxNumber({ firstDay, boxnum, numDays }) {
     if (boxnum >= firstDay && boxnum < numDays + firstDay) {
-        return React.createElement(
-            "p",
-            { className: "boxnum" },
-            boxnum - firstDay + 1
-        );
+        return <p className="boxnum">{boxnum - firstDay + 1}</p>
     } else if (boxnum < firstDay) {
-        return React.createElement(
-            "p",
-            { className: "boxnum", style: { opacity: "0%" } },
-            "Space"
-        );
+        return <p className="boxnum" style={{opacity: "0%"}}>Space</p>
     }
 }
 
-function Events(_ref3) {
-    var data = _ref3.data,
-        boxnum = _ref3.boxnum,
-        firstDay = _ref3.firstDay,
-        longEvents = _ref3.longEvents,
-        lines = _ref3.lines,
-        eventsNotDisplayed = _ref3.eventsNotDisplayed;
+function Events({ data, boxnum, firstDay, longEvents, lines, eventsNotDisplayed }) {
+    const monthDay = boxnum - firstDay + 1;
+    const startDay = new Date(year, month, monthDay, 0, 0, 0,);
+    const endDay = new Date(year, month, monthDay, 23, 59, 59);
+    const eventElements = [];
 
-    var monthDay = boxnum - firstDay + 1;
-    var startDay = new Date(year, month, monthDay, 0, 0, 0);
-    var endDay = new Date(year, month, monthDay, 23, 59, 59);
-    var eventElements = [];
 
-    var _loop = function _loop(event) {
-        var k = eventsNotDisplayed.indexOf(event.id);
+    for (let event of data) {
+        let k = eventsNotDisplayed.indexOf(event.id);
         if (k != -1) {
-            return "continue";
+            continue;
         }
 
-        var startTime = new Date(event.start_time);
-        var endTime = new Date(event.end_time);
-        var add = true;
+        const startTime = new Date(event.start_time);
+        const endTime = new Date(event.end_time);
+        let add = true;
 
-        for (var i = 0; i < lines.length; i++) {
+        for (let i = 0; i < lines.length; i++) {
             if (lines[i] == null) {
                 continue;
             }
-            if (new Date(lines[i].end_time) < startDay) {
+            if ((new Date(lines[i].end_time)) < startDay) {
                 lines[i] = null;
             }
         }
 
         if (endTime > startDay && startTime < endDay) {
-            var _loop2 = function _loop2(longevent) {
+            for (let longevent of longEvents) {
                 if (event.id == longevent.id) {
-                    if (new Date(longevent.end_time) < endDay) {
-                        // if the long event ends today it must be removed from the list
+                    if (new Date(longevent.end_time) < endDay) { // if the long event ends today it must be removed from the list
                         longEvents.splice(longEvents.indexOf(longevent), 1);
-                        React.useEffect(function () {
+                        React.useEffect(() => {
                             shortenElement(longevent.id);
                         }, []);
                     }
                     add = false;
-                    return "break";
-                }
-            };
-
-            var _iteratorNormalCompletion2 = true;
-            var _didIteratorError2 = false;
-            var _iteratorError2 = undefined;
-
-            try {
-                for (var _iterator2 = longEvents[Symbol.iterator](), _step2; !(_iteratorNormalCompletion2 = (_step2 = _iterator2.next()).done); _iteratorNormalCompletion2 = true) {
-                    var longevent = _step2.value;
-
-                    var _ret2 = _loop2(longevent);
-
-                    if (_ret2 === "break") break;
-                }
-            } catch (err) {
-                _didIteratorError2 = true;
-                _iteratorError2 = err;
-            } finally {
-                try {
-                    if (!_iteratorNormalCompletion2 && _iterator2.return) {
-                        _iterator2.return();
-                    }
-                } finally {
-                    if (_didIteratorError2) {
-                        throw _iteratorError2;
-                    }
+                    break;
                 }
             }
 
-            if (add) {
-                // this bit is if the event starts on this day
-                for (var _i = 0; _i < lines.length; _i++) {
-                    if (event.id == 300 || event.id == 303) {}
-                    if (lines[_i] == null) {
-                        lines[_i] = event;
+            if (add) { // this bit is if the event starts on this day
+                for (let i = 0; i < lines.length; i++) {
+                    if (event.id == 300 || event.id == 303) {
+                    }
+                    if (lines[i] == null) {
+                        lines[i] = event;
                         break;
                     }
                 }
-                var index = lines.indexOf(event);
-                var numpixels = index * (0.037 * window.innerHeight);
-                var color = getRandomColor();
+                let index = lines.indexOf(event);
+                let numpixels = index * (0.037 * window.innerHeight);
+                let color = getRandomColor();
                 if (index < 3 && index > -1) {
-                    eventElements.push(React.createElement(
-                        "p",
-                        { key: event.id, id: "event" + event.id, className: "event", style: { marginTop: numpixels + "px", backgroundColor: "" + color } },
-                        event.name
-                    ));
+                    eventElements.push(<p key={event.id} id={`event${event.id}`} className="event" style={{marginTop: `${numpixels}px`, backgroundColor: `${color}`}}>{event.name}</p>);
                 } else {
                     eventsNotDisplayed.push(event.id);
-                    return "continue";
+                    continue;
                 }
             };
 
-            if (endTime > endDay) {
-                // if the event doesn't end on this day
+            if (endTime > endDay) { // if the event doesn't end on this day
                 longEvents.push(event);
                 if (startDay.getDay() === 0) {
-                    var _index = lines.indexOf(event);
-                    var _numpixels = _index * (0.037 * window.innerHeight);
-                    React.useEffect(function () {
-                        wrapToNextLine(event.id, boxnum, _numpixels);
+                    let index = lines.indexOf(event); 
+                    let numpixels = index * (0.037 * window.innerHeight);
+                    React.useEffect(() => {
+                        wrapToNextLine(event.id, boxnum, numpixels);
                     }, []);
                 } else {
-                    React.useEffect(function () {
+                    React.useEffect(() => {
                         widenElement(event.id);
                     }, []);
                 }
-            }
-        }
-    };
-
-    var _iteratorNormalCompletion = true;
-    var _didIteratorError = false;
-    var _iteratorError = undefined;
-
-    try {
-        for (var _iterator = data[Symbol.iterator](), _step; !(_iteratorNormalCompletion = (_step = _iterator.next()).done); _iteratorNormalCompletion = true) {
-            var event = _step.value;
-
-            var _ret = _loop(event);
-
-            if (_ret === "continue") continue;
-        }
-    } catch (err) {
-        _didIteratorError = true;
-        _iteratorError = err;
-    } finally {
-        try {
-            if (!_iteratorNormalCompletion && _iterator.return) {
-                _iterator.return();
-            }
-        } finally {
-            if (_didIteratorError) {
-                throw _iteratorError;
             }
         }
     }
@@ -176,106 +94,63 @@ function Events(_ref3) {
     return eventElements;
 }
 
-function CalendarBoxes(_ref4) {
-    var data = _ref4.data;
-
-    var numBoxes = 42;
-    var boxes = [];
+function CalendarBoxes({data}) {
+    const numBoxes = 42;
+    const boxes = [];
     month = getCookie('month', 10);
     year = getCookie('year'), 10;
-    var longEvents = [];
-    var firstDay = new Date(year, month, 1).getDay() - 1;
+    let longEvents = [];
+    let firstDay = new Date(year, month, 1).getDay() - 1;
     firstDay < 0 ? firstDay += 7 : null;
-    var numDays = new Date(year, month + 1, 0).getDate();
-    var lines = Array(3).fill(null);
-    var eventsNotDisplayed = [];
+    const numDays = new Date(year, month + 1, 0).getDate();
+    let lines = Array(3).fill(null);
+    let eventsNotDisplayed = [];
 
-    for (var i = 0; i < numBoxes; i++) {
-        var style = (i + 1) % 7 === 0 ? { borderRight: 0 } : {};
-        boxes.push(React.createElement(
-            "div",
-            { key: i, className: "calendar-box", style: style, id: "calendar-box" + i },
-            React.createElement(BoxNumber, { firstDay: firstDay, boxnum: i, numDays: numDays }),
-            React.createElement(Events, { data: data, boxnum: i, firstDay: firstDay, longEvents: longEvents, lines: lines, eventsNotDisplayed: eventsNotDisplayed })
-        ));
+    for (let i = 0; i < numBoxes; i++) {
+        const style = (i + 1) % 7 === 0 ? { borderRight: 0 } : {};
+        boxes.push(
+            <div key={i} className="calendar-box" style={style} id={`calendar-box${i}`}>
+                <BoxNumber firstDay={firstDay} boxnum={i} numDays={numDays}/>
+                <Events data={data} boxnum={i} firstDay={firstDay} longEvents={longEvents} lines={lines} eventsNotDisplayed={eventsNotDisplayed}/>
+            </div>
+        );
     }
-    return React.createElement(
-        "div",
-        { id: "calendar-box-container" },
-        boxes
-    );
-}
+    return <div id="calendar-box-container">{boxes}</div>;
+} 
 
 function displayCalendar(data) {
     if (root == null) {
-        var domContainer = document.querySelector('#calendar');
-        root = ReactDOM.createRoot(domContainer);
+        const domContainer = document.querySelector('#calendar');
+        root = createRoot(domContainer);
     }
-    var monthName = getMonthName(getCookie('month'));
-    var year = getCookie('year');
-    root.render(React.createElement(
-        "div",
-        null,
-        React.createElement(
-            "div",
-            { id: "top-bar" },
-            React.createElement(
-                "button",
-                { onClick: prevMonth, id: "backmonth" },
-                React.createElement("img", { id: "backmonthimg", src: "back-arrow.png" })
-            ),
-            React.createElement(Title, { text: monthName + ' ' + year }),
-            React.createElement(
-                "button",
-                { onClick: nextMonth, id: "forwardmonth" },
-                React.createElement("img", { id: "forwardmonthimg", src: "forward-arrow.png" })
-            )
-        ),
-        React.createElement(
-            "ul",
-            { id: "weekday-list" },
-            React.createElement(
-                "li",
-                { className: "weekday-title" },
-                "Mon"
-            ),
-            React.createElement(
-                "li",
-                { className: "weekday-title" },
-                "Tue"
-            ),
-            React.createElement(
-                "li",
-                { className: "weekday-title" },
-                "Wed"
-            ),
-            React.createElement(
-                "li",
-                { className: "weekday-title" },
-                "Thu"
-            ),
-            React.createElement(
-                "li",
-                { className: "weekday-title" },
-                "Fri"
-            ),
-            React.createElement(
-                "li",
-                { className: "weekday-title" },
-                "Sat"
-            ),
-            React.createElement(
-                "li",
-                { className: "weekday-title", style: { borderRight: 0 } },
-                "Sun"
-            )
-        ),
-        React.createElement(CalendarBoxes, { data: data })
-    ));
+    const monthName = getMonthName(getCookie('month'))
+    const year = getCookie('year');
+    root.render(
+        <div>
+            <div id="top-bar">
+                <button onClick={prevMonth} id="backmonth"><img id="backmonthimg" src="back-arrow.png"></img></button>
+                <Title text={monthName + ' ' + year}/>
+                <button onClick={nextMonth} id="forwardmonth"><img id="forwardmonthimg" src="forward-arrow.png"></img></button>
+            </div>
+            <ul id="weekday-list">
+                <li className="weekday-title">Mon</li>
+                <li className="weekday-title">Tue</li>
+                <li className="weekday-title">Wed</li>
+                <li className="weekday-title">Thu</li>
+                <li className="weekday-title">Fri</li>
+                <li className="weekday-title">Sat</li>
+                <li className="weekday-title" style={{ borderRight: 0 }}>Sun</li>
+            </ul>
+            <CalendarBoxes data={data}/>
+        </div>
+    );
 }
 
 function getMonthName(monthNumber) {
-    var monthNames = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+    const monthNames = [
+        "January", "February", "March", "April", "May", "June",
+        "July", "August", "September", "October", "November", "December"
+    ];
     return monthNames[monthNumber];
 }
 
---
+++ display_day1.js
@@ -0,0 +1,98 @@
+var root = null;
+
+function EventList(_ref) {
+    var data = _ref.data;
+
+    var events = [];
+    var year = getCookie('year');
+    var month = getCookie('month');
+    var day = getCookie('day');
+    var start_day = new Date(year, month, day, 0, 0, 0);
+    var end_day = new Date(year, month, day, 23, 59, 59);
+
+    var _iteratorNormalCompletion = true;
+    var _didIteratorError = false;
+    var _iteratorError = undefined;
+
+    try {
+        for (var _iterator = data[Symbol.iterator](), _step; !(_iteratorNormalCompletion = (_step = _iterator.next()).done); _iteratorNormalCompletion = true) {
+            var event = _step.value;
+
+            start_time = new Date(event.start_time);
+            end_time = new Date(event.end_time);
+
+            var formatTime = function formatTime(date) {
+                if (date < start_day) {
+                    return '00:00';
+                }
+                if (date > end_day) {
+                    return '23:59';
+                }
+
+                var hours = date.getHours().toString().padStart(2, '0');
+                var minutes = date.getMinutes().toString().padStart(2, '0');
+                return hours + ':' + minutes;
+            };
+
+            var color = getRandomColor();
+
+            events.push(React.createElement(
+                'div',
+                { className: 'event', key: event.id },
+                React.createElement(
+                    'div',
+                    { className: 'times' },
+                    React.createElement(
+                        'p',
+                        { className: 'start-time' },
+                        formatTime(start_time)
+                    ),
+                    React.createElement(
+                        'p',
+                        { className: 'end-time' },
+                        formatTime(end_time)
+                    )
+                ),
+                React.createElement(
+                    'div',
+                    { className: 'eventBar', id: event.id, 'data-added': event.isaddedevent, style: { backgroundColor: '' + color } },
+                    React.createElement(
+                        'p',
+                        { className: 'eventName' },
+                        event.name
+                    )
+                )
+            ));
+        }
+    } catch (err) {
+        _didIteratorError = true;
+        _iteratorError = err;
+    } finally {
+        try {
+            if (!_iteratorNormalCompletion && _iterator.return) {
+                _iterator.return();
+            }
+        } finally {
+            if (_didIteratorError) {
+                throw _iteratorError;
+            }
+        }
+    }
+
+    return React.createElement(
+        'div',
+        null,
+        events
+    );
+}
+
+function displayDay(data) {
+    if (root == null) {
+        var domContainer = document.getElementById('eventList');
+        root = ReactDOM.createRoot(domContainer);
+    }
+
+    root.render(React.createElement(EventList, { data: data }));
+}
+
+window.displayDay = displayDay;
