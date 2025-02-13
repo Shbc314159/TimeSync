8fdb7872b28d63e225f9ec3cfbf245c865b18a0f
Shbc314159 <shbc99@gmail.com>
2025-02-13 12:41:53 +0000

    change-null

--- login.js
+++ login.js
@@ -1,7 +1,7 @@
 async function startlogin() {
     const username = document.getElementById('username').value;
     const password = document.getElementById('password').value;
-    if (username == null || password == null) {
+    if (username == '' || password == '') {
         alert('Please fill in all fields.');
         return;
     }
